41e42ad8d17cf8d728855b90addfdfd71c717210
Abishek Budhiraja <abishekbudhiraja6@gmail.com>
2026-03-13 14:53:27 +0000

    Updated to v2.0.0 - testing CI/CD auto pipeline

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
 def status():
     return jsonify({
         "app": "Flask CI/CD Demo",
-        "version": "1.0.0",
+        "version": "2.0.0",
         "status": "running",
         "author": "Abishek Budhiraja"
     })
